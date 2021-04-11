48bdbec7d884cc26bd5d1ad1bcef765b2be1d2ff
Bryand700 <55756626+Bryand700@users.noreply.github.com>
2021-04-11 10:29:21 -0600

    Backtracking

--- JavaApplication1/src/AlgortimoFuerzaBruta/Algoritmo.java
+++ JavaApplication1/src/AlgortimoFuerzaBruta/Algoritmo.java
@@ -119,6 +119,171 @@ public class Algoritmo {
     public ArrayList<ArrayList<Carta>> getListaSoluciones() {
         return listaSoluciones;
     }
+    public long Backtracking(){
+        ArrayList<Carta> posibleSolucion;
+        ArrayList<Carta> solucion = mazo.getSolucion();
+        int sospechoso;
+        int arma;
+        int motivo;
+        int parteCuerpo;
+        int lugar;
+        //String salida = "";                   // Almacena el texto que se mostrara al usuario
+        //String tiempoFb;                      // Para retornar el tiempo junto a la salida
+        
+        Carta temp = new Carta("Temporal", Tipo.Arma);
+        posibleSolucion= new ArrayList<>();
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        
+        ArrayList<Carta> mazoSospechoso = mazo.getListaMasos().get(0).getCartas();
+        ArrayList<Carta> mazoArma = mazo.getListaMasos().get(1).getCartas();
+        ArrayList<Carta> mazoMotivo = mazo.getListaMasos().get(2).getCartas();
+        ArrayList<Carta> mazoParteCuerpo = mazo.getListaMasos().get(3).getCartas();
+        ArrayList<Carta> mazoLugar = mazo.getListaMasos().get(4).getCartas();
+        
+        long tiempoInicio = System.nanoTime();
+        long tiempoFinal = 0;
+        for (sospechoso = 0; sospechoso < mazoSospechoso.size(); sospechoso++) {
+            
+            Carta getSospechoso = mazoSospechoso.get(sospechoso);
+            while(!getSospechoso.isCorrecto()&& sospechoso < mazoSospechoso.size()-1 && Poda(posibleSolucion)){
+                sospechoso++;
+                getSospechoso = mazoSospechoso.get(sospechoso);                
+            }
+            posibleSolucion.set(0,getSospechoso);
+            
+            for (arma = 0; arma < mazoArma.size(); arma++){
+                Carta getArma = mazoArma.get(arma);
+                while(!getArma.isCorrecto()&& arma < mazoArma.size()-1 && Poda(posibleSolucion)){
+                    arma++;
+                    getArma = mazoArma.get(arma);                
+                }
+                posibleSolucion.set(1,getArma);
+                
+                for (motivo = 0; motivo < mazoMotivo.size(); motivo++){
+                    Carta getMotivo = mazoMotivo.get(motivo);
+                    while(!getMotivo.isCorrecto()&& motivo < mazoMotivo.size()-1 && Poda(posibleSolucion)){
+                        motivo++;
+                        getMotivo = mazoMotivo.get(motivo);                
+                    }
+                    posibleSolucion.set(2,getMotivo);
+                    
+                    for (parteCuerpo = 0; parteCuerpo < mazoParteCuerpo.size(); parteCuerpo++){
+                        Carta getParteCuerpo = mazoParteCuerpo.get(parteCuerpo);
+                        while(!getParteCuerpo.isCorrecto()&& parteCuerpo < mazoParteCuerpo.size()-1 && Poda(posibleSolucion)){
+                            parteCuerpo++;
+                            getParteCuerpo = mazoParteCuerpo.get(parteCuerpo);                
+                        }
+                        posibleSolucion.set(3,getParteCuerpo);
+                        
+                        for (lugar = 0; lugar < mazoLugar.size(); lugar++){
+                            Carta getLugar = mazoLugar.get(lugar);
+                            while(!getLugar.isCorrecto()&& lugar < mazoLugar.size()-1 && Poda(posibleSolucion)){
+                                lugar++;
+                                getLugar = mazoLugar.get(lugar);                
+                            }
+                            posibleSolucion.set(4,getLugar); 
+                            ArrayList<Carta> tempS = (ArrayList<Carta>) posibleSolucion.clone();
+                            listaSoluciones.add(tempS);
+                            
+                            //salida += posibleSolucion.toString()+"\n";               //Para mostrar en la interfaz
+                            if(posibleSolucion.equals(solucion)){
+
+                                tiempoFinal = System.nanoTime();                
+                                //tiempoFb = String.valueOf(tiempoFinal-tiempoInicio); //Convierte el tiempo a string para poder hacer un solo return
+                                //salida += ";"+tiempoFb;                              //Para este punto, ya tenemos toda la salida
+                                //return tiempoFb;
+                                return tiempoFinal-tiempoInicio;                   //Antiguo return long
+                            }
+                            else{
+                                mazo.MarcarCartaIncorrecta();
+                            }
+                                
+                            
+                        }
+                    }
+                }
+            }
+        }
+        //tiempoFb = "-";
+        //return tiempoFb;
+        return -1;
+    }
+    private boolean Poda(ArrayList<Carta> posibleSolucion){
+        
+        ArrayList<ArrayList<Carta>> listaRestricciones = mazo.getListaRestricciones();
+        for (int i = 0; i < listaRestricciones.size(); i++) {
+            ArrayList<Carta> get = listaRestricciones.get(i);
+            if(posibleSolucion.indexOf(get.get(0))==-1 || posibleSolucion.indexOf(get.get(1)) ==-1)
+                ;
+            else
+                return true;
+        }
+        return false;
+    }
     
+    public long BacktrackingRecursivo(){
+        ArrayList<Carta> posibleSolucion;
+        ArrayList<Carta> solucion = mazo.getSolucion();
+        
+        Carta temp = new Carta("Temporal", Tipo.Arma);
+        posibleSolucion= new ArrayList<>();
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        posibleSolucion.add(temp);
+        
+        ArrayList<Carta> mazoSospechoso = mazo.getListaMasos().get(0).getCartas();
+        ArrayList<Carta> mazoArma = mazo.getListaMasos().get(1).getCartas();
+        ArrayList<Carta> mazoMotivo = mazo.getListaMasos().get(2).getCartas();
+        ArrayList<Carta> mazoParteCuerpo = mazo.getListaMasos().get(3).getCartas();
+        ArrayList<Carta> mazoLugar = mazo.getListaMasos().get(4).getCartas();
+        
+        ArrayList<ArrayList<Carta>> mazos= new ArrayList<>();
+        mazos.add(mazoSospechoso);
+        mazos.add(mazoArma);
+        mazos.add(mazoMotivo);
+        mazos.add(mazoParteCuerpo);
+        mazos.add(mazoLugar);
+        
+        long tiempoInicio = System.nanoTime();
+        long tiempoFinal = auxiliar(posibleSolucion, mazos, 0, solucion);
+        if(tiempoFinal >-1)
+            return tiempoFinal-tiempoInicio;
+        return -1;
+    }
     
+    private long auxiliar(ArrayList<Carta> posibleSolucion, ArrayList<ArrayList<Carta>> mazos, int i
+            ,ArrayList<Carta> solucion){
+        
+        ArrayList<Carta> mazoActual = mazos.get(i);
+        for (int j = 0; j < mazoActual.size(); j++) {
+            Carta get = mazoActual.get(j);
+            while(!get.isCorrecto()&& j < mazoActual.size()-1 && Poda(posibleSolucion)){
+                j++;
+                get = mazoActual.get(j);                
+            }
+            posibleSolucion.set(i,get);
+            if(posibleSolucion.equals(solucion)){
+
+                long tiempoFinal = System.nanoTime();                
+                return tiempoFinal;                   
+            }
+            else{
+                mazo.MarcarCartaIncorrecta();
+                if(i<4){                    
+                    long tiempo = auxiliar(posibleSolucion,mazos,i+1,solucion);  
+                    if (tiempo != -1) {
+                        return tiempo;                        
+                    }
+                }
+                
+            }
+        }
+        return -1;
+    }
 }
